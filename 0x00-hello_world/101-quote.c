1bbd25c32133596493d330fa68654d4c4194d9a2
johnconnor77 <jfcalleh@unal.edu.co>
2019-06-19 23:14:08 +0000

    Advanced 09

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -8,6 +9,6 @@ include <unistd.h>
  */
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
