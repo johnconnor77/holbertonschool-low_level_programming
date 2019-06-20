ea8b80b17b7a9caadbd5d854ee32667aac2b3eaa
johnconnor77 <jfcalleh@unal.edu.co>
2019-06-20 19:55:29 +0000

    Updated 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alfa;
+for (alfa = 'z'; alfa >= 'a'; alfa--)
+putchar(alfa);
+putchar('\n');
+return (0);
+}
