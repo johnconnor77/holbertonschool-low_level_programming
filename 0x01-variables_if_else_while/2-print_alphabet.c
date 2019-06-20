0f4a4a494ceae7f8cc5adc30409e9e069d05dae9
johnconnor77 <jfcalleh@unal.edu.co>
2019-06-20 17:03:30 +0000

    Update 0-2 scripts

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+char alfa;
+for (alfa = 'a'; alfa <= 'z'; ++alfa)
+putchar(alfa);
+putchar('\n');
+return (0);
+}
