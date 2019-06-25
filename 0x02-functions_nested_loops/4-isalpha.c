379f6c94659cdea86859fe50a20172c06eca92f8
johnconnor77 <jfcalleh@unal.edu.co>
2019-06-25 18:13:28 +0000

    function that checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _islower - check on lowercase values.
+ * @c: input value for checking restrictions
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+int val;
+if (c >= 97 && c <= 122)
+{
+	val = 1;
+}
+else if (c >= 65 && c <= 90)
+{
+	val = 1;
+}
+else
+{
+	val = 0;
+}
+return (val);
+}
