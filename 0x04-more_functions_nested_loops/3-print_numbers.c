ebaaba777faf3dc9446ff631c062b5cfe3e6caf5
johnconnor77 <jfcalleh@unal.edu.co>
2019-06-27 19:35:33 +0000

    Updated 3 function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_numbers -prints digits 0 to 9
+ *
+ *Return:void
+ */
+void print_numbers(void)
+{
+int num;
+
+for (num = 0; num <= 9; num++)
+{
+	_putchar(num + '0');
+
+}
+_putchar('\n');
+
+return;
+}
+
