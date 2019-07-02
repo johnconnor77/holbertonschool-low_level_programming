82ce368d630711fa06c3da6065392fecbd2d6819
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-02 21:01:11 +0000

    Upload 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *rev_string - function that print in revrse
+ * the input string
+ *@s: string input
+ *
+*/
+void rev_string(char *s)
+{
+int i, j, k, m;
+char aux[100];
+m = 0;
+k = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+for (j = i - 1 ; s[j] != '\0'; j--)
+{
+ 	aux[m] = s[j];
+m++;
+}
+for (k = 0; s[k] != '\0'; k++)
+{
+	s[k] = aux[k];
+}
+}
+
