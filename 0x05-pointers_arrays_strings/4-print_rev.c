ff5380cbfc21429d1e7541f0994b54211a4371b5
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-02 19:06:15 +0000

    Upload 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_rev - function that print in revrse
+ * the input string
+ *@s: string input
+ *
+*/
+void print_rev(char *s)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+for (j = i - 1 ; s[j] != '\0'; j--)
+{
+	_putchar(s[j]);
+}
+}
+
