5524859c88ac6828bf08a20315867773ad6c859d
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-02 22:25:14 +0000

    Corrected 5

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
  *puts2 - function that prints jumps 2 string inlet
- *@s: string input
+ *@str: string input
 */
 
-void puts2(char *s)
+void puts2(char *str)
 {
 int i;
-for (i = 0; s[i] != '\0'; i += 2)
+for (i = 0; str[i] != '\0'; i += 2)
 {
-_putchar(s[i]);
+_putchar(str[i]);
 }
 _putchar('\n');
 }
