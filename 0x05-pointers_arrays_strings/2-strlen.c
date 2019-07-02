7da3540d1cb2d044e32d6c5d76cd93a31f653215
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-02 18:47:32 +0000

    Upload 2-3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *_strlen - function that calculate the lenght
+ * of the input string
+ *@s: string input
+ *Return: lenght of string
+*/
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
+
