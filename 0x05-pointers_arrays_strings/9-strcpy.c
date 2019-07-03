2d993006186d49b81d8c628d18b358a8f5efb023
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-03 04:57:06 +0000

    Corrected 7 & 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *_strcpy - put a string pointer into other one
+ *Result: dest
+ *@dest:string inlet
+ *@src: string outlet
+ *Return: value depending on function
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
