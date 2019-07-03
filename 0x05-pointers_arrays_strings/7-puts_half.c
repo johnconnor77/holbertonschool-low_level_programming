e9ea6ae582c1e4c6c5cd1eecbb78790db3067bf9
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-03 04:32:26 +0000

    Upload 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *puts_half - function that prints last half string inlet
+ *@s: string input
+*/
+
+void puts_half(char *s)
+{
+int i, j, half;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+
+if (i % 2 == 0)
+{
+	half = i / 2;
+}
+else
+{
+	half = (i - 1) / 2;
+}
+for (j = half; s[j] != '\0'; j++)
+{
+	_putchar(s[j]);
+}
+_putchar('\n');
+}
+
