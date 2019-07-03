20fb45105d2eaaba145f724141c1495bcfc98e99
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-03 12:47:41 +0000

    9 Corrected

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,6 +8,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+int i, j;
+for (i = 0; src[i] != '\0'; i++)
+{
+}
+for (j = 0; j < i; j++)
+{
+	dest[j] = src[j];
+}
+dest[i] = src[i];
+
 	return (dest);
 }
