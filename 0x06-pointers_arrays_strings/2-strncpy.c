057a345e7afbf200e6743b72a5a1b8b27c881f92
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-04 17:09:26 +0000

    Upload 0-2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,36 @@
+#include"holberton.h"
+/**
+ *_strncpy -function that copies a string.
+ *@src: string input
+ *@dest: output from function where its replaced 
+ *@n:size of bytes to print
+ *Return: dest string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int leng, i;
+
+leng = _strlen(dest);
+
+for (i = 0; (i < n && src[i] != '\0'); i++)
+	dest[leng + i] = src[i];
+
+dest[leng + i] = '\0';
+
+return (dest);
+}
+/**
+ *_strlen - function that calculate the lenght
+ * of the input string
+ *@s: string input
+ *Return: lenght of string
+*/
+int _strlen(char *s)
+{
+auto int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
+
