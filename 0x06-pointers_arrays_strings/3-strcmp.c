aed70e183b83007a83cfd0dca37c3c572bd24082
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-04 20:18:06 +0000

    Corrected 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,17 +6,16 @@
  *@s2: input string
  *Return: diff from strings following ASCII
 */
+
 int _strcmp(char *s1, char *s2)
 {
-int dif;
-dif = 0;
-
 while ( *s1 != '\0' || *s2 != '\0')
 {
-	dif += *s1 - *s2;
+	if (*s1 != *s2)
+		return (*s1 - *s2);
 	s1++;
 	s2++;
 }
-return (dif);
+return (0);
 }
 
