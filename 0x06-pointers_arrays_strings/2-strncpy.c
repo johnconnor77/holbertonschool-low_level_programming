0b69ad2888f95a782f57807fc414e7ed94a30bed
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-04 18:28:10 +0000

    Upload 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,16 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int leng, i;
+int i;
 
-leng = _strlen(dest);
 
 for (i = 0; (i < n && src[i] != '\0'); i++)
-	dest[leng + i] = src[i];
+	dest[i] = src[i];
 
-dest[leng + i] = '\0';
+for ( ; i < n; i++)
+{
+dest[i] = '\0';
+}
 
 return (dest);
 }
