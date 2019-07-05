1eacc3b606c63ca63802ad6688657d0fd90f487e
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-05 03:11:00 +0000

    6 Corrected

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * cap_string - function that capitalizes all worlds of a string
@@ -10,21 +11,23 @@ char *cap_string(char *s)
 {
 	int i;
 	int j;
-	int sep[] = {' ', '\t', '\n', ',', ';', '.',
-		 '!', '?', '"', '(', ')', '{', '}', '\0'};
-	i = 0;
-	j = 0;
+	int sep[14] = {' ', '\t', '\n', ',', ';', '.',
+			 '!', '?', '"', '(', ')', '{', '}', '\0'};
+	printf("%d\n", s[6]);
+	printf("%d\n", sep[1]);
+	if (sep[1] == s[6])
+		printf("CIERTO");
+	i = 1;
 	if (s[0] >= 'a' && s[0] <= 'z')
 	{
 		s[0] = s[0] - 32;
 	}
 	while (s[i] != '\0')
 	{
-	
+	j=0;
 		while (sep[j] != '\0')
 		{
-		
-			if (s[i] == sep[j])
+			if (sep[j] == s[i])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
@@ -35,6 +38,5 @@ char *cap_string(char *s)
 		}
 	i++;
 	}
-
 return (s);
 }
