29d7ab2e371ef584951b6a1a55628033c29bd01a
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-08 22:26:24 +0000

    Upload 4-5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include"holberton.h"
+/**
+ *_strpbrk -function that searches a string for any of a set of bytes
+ *@needle: string inlet
+ *@haystack: match statement string
+ *Return: s
+**/
+char *_strstr(char *haystack, char *needle)
+{
+int k, i, j, flag;
+
+flag = 0;
+
+for (k = 0; needle != '\0',k++)
+{
+}
+
+
+for (j = 0; haystack[j] != '\0'; j++)
+{
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (haystack[j] == accept[i])
+		{
+			flag ++;
+		}
+	
+	}
+if (flag == k)
+{
+	break;
+}
+else
+}
+return (haystack);
+}
