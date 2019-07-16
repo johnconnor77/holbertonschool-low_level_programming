a2d57b8c472b11885988a137faf670cb43a71ee4
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-16 20:13:18 +0000

    Upload 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
+ * @s1: output with original content of s1 and & with s2
+ * @s2: string for concatenate
+ * Return: s1 output from s1 + s2
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	unsigned int i, j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	s = malloc(i + j);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		s[i + j] = s2[j];
+
+return (s);
+}
+
