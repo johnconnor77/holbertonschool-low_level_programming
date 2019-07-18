b5160c15756d07c6b01a59a567cb884172b2aa2e
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-18 20:36:41 +0000

    Upload 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,6 +14,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
