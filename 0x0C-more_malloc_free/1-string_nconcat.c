7fd420bdb26af92e5e083f83ec382523b588cdc2
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-18 19:18:52 +0000

    Corrected limit for 2nd j loop

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,10 +40,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		/* reboot i & j*/
 
 	for (i = 0; s1[i] != '\0'; i++)
-		d[i] = s1[i]; 
+		d[i] = s1[i];
 
 	for (j = 0; j < n; j++)
-		d[i + j] = s2[j]; 
+		d[i + j] = s2[j];
 
 	d[i + j] = '\0';
 
