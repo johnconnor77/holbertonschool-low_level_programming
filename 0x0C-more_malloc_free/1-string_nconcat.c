1abdcab2355b4e432553b5972dde1dc685469dd6
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-18 19:16:28 +0000

    Upload 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		d[i] = s1[i]; 
 
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < n; j++)
 		d[i + j] = s2[j]; 
 
 	d[i + j] = '\0';
