460c23ee0cfb8473cc84caa6523f1fc1811c71e0
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-22 17:17:05 +0000

    Corrected for case in Null d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - function that initialize a variable of type struct dog
@@ -9,6 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return ;
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
