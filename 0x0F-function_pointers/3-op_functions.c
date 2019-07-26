d17f45a381038ad52608755f0a884bf97efcb6f6
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-26 06:15:58 +0000

    Contain the operative functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+ * op_add - function that calculates the sum of  a and b
+ *
+ * @a: input int
+ * @b: input int
+ *
+ * Return: addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that calculates the substraction a and b
+ *
+ * @a: input int
+ * @b: input int
+ *
+ * Return: substraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that calculates the product of a and b
+ *
+ * @a: input int
+ * @b: input int
+ *
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function that calculates the division of a and b
+ *
+ * @a: input int
+ * @b: input int
+ *
+ * Return: Division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function that calculates the module a and b
+ * @a: input int
+ * @b: input int
+ *
+ * Return: Module
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
