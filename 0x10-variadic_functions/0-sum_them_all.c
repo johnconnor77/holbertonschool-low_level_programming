d40a47a9b1ffb7ee91641eb1d79bcfd39337105d
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-31 19:50:47 +0000

    Fixed Troubles on ooutput sums

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,26 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - function that return the sum of all its parameters
- * @n: value
+ * @n: amount of parameters
  * Return: sum of all arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list vlist;
-	int i;
+	va_list arglist;
+	unsigned int i;
 	int sum;
 
 	if (n == 0)
 		return (0);
 
-	va_start(vlist, n); %
+	va_start(arglist, n); 
 
 	sum = 0;
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(vlist, int);
+		sum += va_arg(arglist, int);
 
-	va_end(vlist);
+	va_end(arglist);
 	return (sum);
 }
+
