2d572cb82c1dd4c3ca7c45ae6c0f3f45e3b16e69
johnconnor77 <jfcalleh@unal.edu.co>
2019-07-31 21:00:15 +0000

    Header with fixed id pointers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,22 +9,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 /**
  * struct print_type - structure for different types
- * @ro: formatted character c,i,f,s
+ * @id: identifier on formatted character c,i,f,s
  * @f: function pointer to list
  *
  */
 typedef struct print_type
 {
-	char *ro;
+	char *id;
 	void (*f)(va_list); /* function pointer */
 } pt_t;
 
-void print_c(va_list risuto);
-void print_i(va_list risuto);
-void print_f(va_list risuto);
-void print_s(va_list risuto);
+void print_c(va_list inlet);
+void print_i(va_list inlet);
+void print_f(va_list inlet);
+void print_s(va_list inlet);
 
 #endif
