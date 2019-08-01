569e19822b63ddf9b68398ecbe4acdb0b73ee60c
johnconnor77 <jfcalleh@unal.edu.co>
2019-08-01 20:48:06 +0000

    function that adds a new node at the end of a list_t list.

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * add_node -function that add a new node
+ * at the beginning of a list
+ * @head: start of linked list
+ * @str: string that will be added
+ * Return: address
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+	int i = 0, len = 0;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+		return (newnode);
+
+	newnode->str = strdup(str); /*From string.h */
+
+	if (newnode->str == NULL)
+	{
+		free(newnode);
+		return (newnode);
+	}
+	
+	while(str[i] != '\0')
+	{
+	len++;
+	i++;
+	}
+
+	newnode->len = len;
+
+	newnode->next = *head;
+
+	*head = newnode
+
+	return (*head);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * add_node_end - function add a new node to the head
+ * of a linked list
+ *
+ * @head: start of linked list
+ * @str: string that will be added
+ *
+ * Return: count, number of nodes
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode;
+	list_t *temp;
+	int i;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = strdup(str);
+
+	newnode->next = NULL;
+
+	temp = *head;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	newnode->len = i;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+
+	while(temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = newnode;
+
+	return (newnode);
+}
