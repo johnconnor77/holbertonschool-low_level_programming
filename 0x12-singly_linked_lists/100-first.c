75ebc973c748c7724a2dc488a712c9dcef5785b4
johnconnor77 <jfcalleh@unal.edu.co>
2019-08-02 05:51:57 +0000

    Function that prints prev to main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+* prev_main - function thats bein exe prev to main
+*/
+
+void prev_main(void)
+{
+	char *s = "I bore my house upon my back!\n";
+
+	printf("You're beat! and yet, you must allow,\n%s", s);
+}
+
