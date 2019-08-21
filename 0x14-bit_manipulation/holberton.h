2735766d0dcb62e4390a3fa938a41496e1398f52
johnconnor77 <jfcalleh@unal.edu.co>
2019-08-21 04:08:50 +0000

    Update Header file

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,9 +1,11 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 #include <stdio.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 int get_endianness(void);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
