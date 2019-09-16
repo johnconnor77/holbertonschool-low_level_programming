81cc73235881695e53b7c16d36f72ff71648b01b
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-16 05:17:25 +0000

    Fixed Betty issues

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_dlistint  function that- returns the sum
+ * sum_dlistint - function that- returns the sum
  * of all data(n) of a doubly linked list
  * @head: pointer to the head of the double linked list
  * Return: sum or 0 if the list is empty
