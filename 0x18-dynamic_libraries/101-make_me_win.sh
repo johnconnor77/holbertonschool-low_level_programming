ac68399e7ff21790dd642e381b2c2c086368aa15
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-17 02:09:01 +0000

    Corrected function of puts in dynamic lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/johnconnor77/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/0x17.c/mkw.so
+wget -P ../ https://github.com/johnconnor77/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/0x17.c/jackpot.so 
 export LD_PRELOAD=../mwk.so
