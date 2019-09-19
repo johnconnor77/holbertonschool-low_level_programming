f660040425cfe699be1d7d5a6cd0e47d35364b71
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-19 04:33:39 +0000

    Update Script for corrupt folder

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/johnconnor77/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/0x17.c/jackpot.so 
+wget -P ../ https://github.com/johnconnor77/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/0x17/jackpot.so 
 export LD_PRELOAD=../jackpot.so
