b1319cb3858e196dbfa6f2908e8eefa8540eae20
johnconnor77 <jfcalleh@unal.edu.co>
2019-10-25 17:23:02 -0500

    Clion complements

---
+++ 0x1B-sorting_algorithms/cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/eltueto/Desktop/holbertonschool-low_level_programming/0x1B-sorting_algorithms/0-main.c" "/home/eltueto/Desktop/holbertonschool-low_level_programming/0x1B-sorting_algorithms/cmake-build-debug/CMakeFiles/main.dir/0-main.c.o"
+  "/home/eltueto/Desktop/holbertonschool-low_level_programming/0x1B-sorting_algorithms/print_array.c" "/home/eltueto/Desktop/holbertonschool-low_level_programming/0x1B-sorting_algorithms/cmake-build-debug/CMakeFiles/main.dir/print_array.c.o"
+  "/home/eltueto/Desktop/holbertonschool-low_level_programming/0x1B-sorting_algorithms/print_list.c" "/home/eltueto/Desktop/holbertonschool-low_level_programming/0x1B-sorting_algorithms/cmake-build-debug/CMakeFiles/main.dir/print_list.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 0x1B-sorting_algorithms/cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/0-main.c.o"
+  "CMakeFiles/main.dir/print_array.c.o"
+  "CMakeFiles/main.dir/print_list.c.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
