c77aa2ca0bbdc78e65026466d0b476054726feda
johnconnor77 <jfcalleh@unal.edu.co>
2019-10-28 15:27:54 -0500

    Fix betty issues

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,34 +1,31 @@
 #include "sort.h"
+
 /**
  * bubble_sort - function that sorts array of int
  * with bubble method
  * @array: list to be sorted
  * @size: length og array inlet
  */
-void bubble_sort(int *array, size_t size)
-{
-    unsigned int j = 0, i = 0;
-    int aux = 0;
+void bubble_sort(int *array, size_t size) {
+	unsigned int j = 0, i = 0;
+	int aux = 0;
 
 
-    if (size < 2 || !array)
-        return;
+	if (size < 2 || !array)
+		return;
 
-    while (i < size - 1)
-    {
-        j =  0;
-        while (j < size - 1)
-        {
-            if (array[j] > array[j + 1])
-            {
-                aux = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = aux;
-                print_array(array, size);
-            }
-            j++;
-        }
-        i++;
-    }
+	while (i < size - 1) {
+		j = 0;
+		while (j < size - 1) {
+			if (array[j] > array[j + 1]) {
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
+			}
+			j++;
+		}
+		i++;
+	}
 
 }
