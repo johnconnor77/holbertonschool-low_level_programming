af4d3a21c47cc5ec53f8823f8ad5277b2a19c400
johnconnor77 <jfcalleh@unal.edu.co>
2019-10-28 17:19:09 -0500

    Update header file

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -23,5 +23,9 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void q_sort(int *array, int lo, int hi, size_t size);
+void swap_vals(int *ai, int *aj);
 
 #endif /* _SORT_H */
