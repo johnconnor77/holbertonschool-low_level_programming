d13a82f973ef234dcc59a73fd444b6fbb0dffb24
johnconnor77 <jfcalleh@unal.edu.co>
2019-10-31 11:47:39 -0500

    function that sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,5 +1,7 @@
 #ifndef _SORT_H
 #define _SORT_H
+#define UNSORTED 0
+#define SORTED 1
 
 #include <stddef.h>
 
@@ -28,5 +30,7 @@ int partition(int *array, int lo, int hi, size_t size);
 void q_sort(int *array, int lo, int hi, size_t size);
 void shell_sort(int *array, size_t size);
 void swap_vals(int *ai, int *aj);
+void swap(listint_t *left, listint_t *right, listint_t **list);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* _SORT_H */
