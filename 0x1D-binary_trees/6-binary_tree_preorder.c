f570716b32c33b25093641ac69529edb27a71fea
johnconnor77 <jfcalleh@unal.edu.co>
2019-12-04 18:33:24 -0500

    function that goes through a binary tree using pre-order traversal

---
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - function that goes
+ * through a binary tree using pre-order traversal
+ * @tree: pointer with bintree address
+ * @func: addres of function pointer for print
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+
+	if (tree->left)
+		binary_tree_preorder(tree->left, func);
+	if (tree->right)
+		binary_tree_preorder(tree->right, func);
+}
