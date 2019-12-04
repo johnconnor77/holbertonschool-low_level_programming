ab930baa48a4bd2ebd51bfc3e5d174b7ae2aef72
johnconnor77 <jfcalleh@unal.edu.co>
2019-12-04 15:20:36 -0500

    Add left reference as NULL

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -1,28 +1,30 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - function that inserts a node as the right-child of another node
- * @param pointer to parent node
- * @param value inside each node
- * @return node address
+ * binary_tree_insert_right - function that inserts a node
+ * as the right-child of another node
+ * @parent: pointer to parent node
+ * @value: integer inside each node
+ * Return: node address
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
 	if (!parent)
-		return NULL;
+		return (NULL);
 
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
-		return NULL;
+		return (NULL);
 
 	node->n = value;
 	node->parent = parent;
 	node->right = parent->right;
 	parent->right = node;
+	node->left = NULL;
 
 	if (node->right)
 		node->right->parent = node;
 
-	return(node);	
+	return (node);
 }
