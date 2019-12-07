63508c1ed87a7545c3f7a003a53299ff4eefd8a0
johnconnor77 <jfcalleh@unal.edu.co>
2019-12-06 21:20:22 -0500

    Update header file

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -54,6 +54,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
-
+void level_printer(const binary_tree_t *tree, void (*func)(int), size_t level);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* HOLBERTONSCHOOL_LOW_LEVEL_PROGRAMMING_BINARY_TREES_H */
