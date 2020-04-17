07ecd8cfe992d895b2a34e0866dec8d00f4e7193
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-16 21:10:18 -0500

    Upload Header file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,4 @@
+//
+// Created by eltueto on 16/04/20.
+//
+
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
+#define INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif //INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
