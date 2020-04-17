2c0dc51f1797518bed8d60e8382c3204f21252aa
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-16 21:28:02 -0500

    fixed some issues for C90 runtime

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,9 @@
-#ifndef INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
-#define INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 
-#endif //INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
+#endif /* SEARCH_ALGOS_H */
