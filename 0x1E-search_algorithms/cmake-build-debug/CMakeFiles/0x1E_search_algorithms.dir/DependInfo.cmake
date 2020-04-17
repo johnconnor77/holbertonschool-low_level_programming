f02e01f14fd3f318834ebae4cab007384c9f3bfe
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-16 21:14:19 -0500

    Clion aux files for runtime

---
+++ 0x1E-search_algorithms/cmake-build-debug/CMakeFiles/0x1E_search_algorithms.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/eltueto/Documents/Holberton/holbertonschool-low_level_programming/0x1E-search_algorithms/0-linear.c" "/home/eltueto/Documents/Holberton/holbertonschool-low_level_programming/0x1E-search_algorithms/cmake-build-debug/CMakeFiles/0x1E_search_algorithms.dir/0-linear.c.o"
+  "/home/eltueto/Documents/Holberton/holbertonschool-low_level_programming/0x1E-search_algorithms/0-main.c" "/home/eltueto/Documents/Holberton/holbertonschool-low_level_programming/0x1E-search_algorithms/cmake-build-debug/CMakeFiles/0x1E_search_algorithms.dir/0-main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 0x1E-search_algorithms/cmake-build-debug/CMakeFiles/0x1E_search_algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "0x1E_search_algorithms"
+  "0x1E_search_algorithms.pdb"
+  "CMakeFiles/0x1E_search_algorithms.dir/0-linear.c.o"
+  "CMakeFiles/0x1E_search_algorithms.dir/0-main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/0x1E_search_algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
